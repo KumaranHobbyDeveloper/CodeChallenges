cf107c5be594e14c96fc3842e2ba43108e6177c4
KumaranSelvaraj <kumaran.selvaraj@live.com>
2017-11-03 15:27:36 -0400

    The code does a recursive reverse the content of a singlly linkedlist.

--- RevrseSinglyLinkedList/Program.cs
+++ RevrseSinglyLinkedList/Program.cs
@@ -28,23 +28,35 @@ namespace RevrseSinglyLinkedList
                 }
             };
             Node reversedNode = null;
-            reversedNode = ReverseRecursive(currentNode,ref reversedNode);
+            ReverseRecursive(currentNode,ref reversedNode);
+            while (reversedNode.Next != null)
+            {
+                Console.WriteLine(reversedNode.Value);
+                reversedNode = reversedNode.Next;
+            }
 
+            Console.Read();
         }
 
-        public static Node ReverseRecursive(Node oldNode,ref Node newNode)
+        public static void ReverseRecursive(Node oldNode,ref Node reverseNode)
         {
+            Node current = null;
             if (oldNode.Next != null)
-                ReverseRecursive(oldNode.Next,ref newNode);
+            {
+                ReverseRecursive(oldNode.Next,ref current);
+            }
+            Node newNode = new Node { Value = oldNode.Value, Next = null };
+            if (current == null)
+            {
+                current = newNode;
+                reverseNode = current;
+            }
             else
             {
-                if (newNode == null)
-                    newNode = oldNode;
-                else
-                    newNode.Next = oldNode;
-                oldNode = null;
+                current.Next = newNode;
+                reverseNode = current.Next;
             }
-            return newNode;
+            //Console.WriteLine(reverseNode.Value);
         }
     }
 
@@ -52,5 +64,6 @@ namespace RevrseSinglyLinkedList
     {
         public string Value;
         public Node Next = null;
+        
     }
 }
